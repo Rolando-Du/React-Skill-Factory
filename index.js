7cdaf1e36b24ee1858ca08fd1262f2afcd733e6a
Rolando Duarte <rolandoduarte83@gmail.com>
2022-07-20 00:34:43 -0300

    new: uploading javascript test

---
+++ index.js
@@ -0,0 +1,26 @@
+const firstname = "Rolando";
+console.log(firstname);
+
+const age = 44;
+console.log(age);
+
+const isMarried = true;
+console.log(isMarried);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers);
+
+const skillers = ["Fernado", "Ezequiel", "Daniel", "Gisele", "Javier"];
+console.log(skillers);
+
+const persons = {
+    firstname: "Rolando",
+    age: 44,
+    isMarried: true,
+    skillers: ["Fernado", "Ezequiel", "Daniel", "Gisele", "Javier"],
+    numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+}
+
+console.log(persons);
+console.log(typeof persons);
+console.log(typeof skillers);
